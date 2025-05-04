f0c57038998596418597ed4ecd41837d4ff88825
Mike Rybka <merybka@gmail.com>
2025-05-03 22:13:16 -0400

    Add types

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/mikerybka/brass"
+	"github.com/mikerybka/english"
 )
 
 //go:embed icon.png
@@ -17,7 +18,34 @@ func main() {
 		Name:     "Schema.cafe",
 		Icon:     icon,
 		CoreType: "schema",
-		Types:    map[string]brass.Type{},
+		Types: map[string]brass.Type{
+			"schema": brass.Type{
+				Name:       english.NewName("Schema"),
+				PluralName: english.NewName("Schemas"),
+				IsStruct:   true,
+				Fields: []brass.Field{
+					{
+						Name:   english.NewName("Fields"),
+						TypeID: "[]field",
+					},
+				},
+			},
+			"field": brass.Type{
+				Name:       english.NewName("Field"),
+				PluralName: english.NewName("Fields"),
+				IsStruct:   true,
+				Fields: []brass.Field{
+					{
+						Name:   english.NewName("Name"),
+						TypeID: "string",
+					},
+					{
+						Name:   english.NewName("Type"),
+						TypeID: "string",
+					},
+				},
+			},
+		},
 	}
 	err := app.GenerateSourceCode(dir)
 	if err != nil {
